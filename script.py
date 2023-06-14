076a9629c44cf4ab9d229cdb8886c58a0d149865
Marat-Tim <rurikcat@gmail.com>
2023-06-14 23:14:43 +0300

    Добавил в README описание и результаты

--- script.py
+++ script.py
@@ -6,16 +6,16 @@ df = pd.read_csv("./vertices.csv", sep=";", index_col="index")
 graph_types = ["полный граф", "дерево", "средне-связный граф"]
 finders = ["Флойд-Уоршелл", "Дейкстра", "Беллман-Форд"]
 
-max_time = df.max().max()
-fig, axs = plt.subplots(figsize=(20, 20), nrows=len(finders), ncols=len(graph_types))
+fig, axs = plt.subplots(figsize=(7, 21), nrows=len(finders))
 for i in range(len(finders)):
-    for j in range(len(graph_types)):
-        axs[i][j].plot(df[f"{finders[i]} + {graph_types[j]}"])
-        axs[i][j].set_title(f"{finders[i]} + {graph_types[j]}")
-        axs[i][j].set_xlabel("Количество вершин в графе")
-        axs[i][j].set_ylabel("Время работы в наносекундах")
-        axs[i][j].grid()
-fig.savefig("image/separately.png")
+    for type in graph_types:
+        axs[i].plot(df[f"{finders[i]} + {type}"], label=type)
+        axs[i].legend()
+        axs[i].set_title(finders[i])
+        axs[i].set_xlabel("Количество вершин в графе")
+        axs[i].set_ylabel("Время работы в наносекундах")
+        axs[i].grid()
+fig.savefig("image/aggregated_by_finders.png")
 
 fig, axs = plt.subplots(figsize=(7, 21), nrows=len(graph_types))
 for i in range(len(graph_types)):
@@ -26,4 +26,4 @@ for i in range(len(graph_types)):
         axs[i].set_xlabel("Количество вершин в графе")
         axs[i].set_ylabel("Время работы в наносекундах")
         axs[i].grid()
-fig.savefig("image/aggregated.png")
+fig.savefig("image/aggregated_by_types.png")
